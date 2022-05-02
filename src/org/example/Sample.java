ada4484a5a63f096b0152ac450ef05e9a68941f3
SubashGitLogin <subash007official@gmail.com>
2022-05-02 06:09:14 -0700

    Add new codes

---
+++ src/org/example/Sample.java
@@ -0,0 +1,31 @@
+package org.example;
+
+public class Sample {
+
+private void empID() {
+	System.out.println("Emp ID :555");
+}
+private void empName() {
+	System.out.println("Emp Name :Subash");
+}
+public static void main(String[] args) {
+	Sample s=new Sample();
+	s.empID();
+	s.empName();
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
