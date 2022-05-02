a8c1328ffe4b7ac002d802400d517cd380e8c647
SubashGitLogin <subash007official@gmail.com>
2022-05-02 06:15:13 -0700

    Add some Code

--- src/org/example/Sample.java
+++ src/org/example/Sample.java
@@ -2,11 +2,17 @@ package org.example;
 
 public class Sample {
 
+	int a=100;
+	int b=50;
+	
 private void empID() {
 	System.out.println("Emp ID :555");
 }
 private void empName() {
 	System.out.println("Emp Name :Subash");
+}
+private void companyName() {
+	System.out.println("Company Name :TCS");
 }
 public static void main(String[] args) {
 	Sample s=new Sample();
