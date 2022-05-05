0ba3d318ff36e8a7ce2ec794e4e6bce615e37ad6
SubashGitLogin <subash007official@gmail.com>
2022-05-05 08:42:21 -0700

    B add Some cade

--- src/org/data/Datas.java
+++ src/org/data/Datas.java
@@ -9,10 +9,13 @@ public class Datas {
 	private void emp(int id) {
 		System.out.println("Emp ID :"+id);
 
+	}private void salary(double sal) {
+		System.out.println("Salary :"+sal);
 	}
 	public static void main(String[] args) {
 		Datas d=new Datas();
 		d.emp();
 		d.emp(555);
+		d.salary(65432643.2351);
 	}
 }
